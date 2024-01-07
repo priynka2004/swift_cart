a31a714987c3c353c16e65416cb3ec5056447e8f
Priyanka Sheoran <priynka.sheoran2004@gmail.com>
2024-01-07 21:14:33 +0530

    Add ConstantClass for global constants

---
+++ lib/utils/app_constant.dart
@@ -0,0 +1,12 @@
+
+
+import 'dart:ui';
+
+class AppConstant{
+  static String appMainName ='SwiftCart';
+  static String appPoweredBy ='Powered ByCodexia';
+  static const appMainColor = Color(0xFFbf1b08);
+  static const appScendoryColor = Color(0xFF981206);
+  static const appTextColor = Color(0xFFFBF5F4);
+  static const appStatusBarColor = Color(0xFFFBF5F4);
+}
