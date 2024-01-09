551da1a24150327e2ebc3c054b45d940966766eb
Priyanka Sheoran <priynka.sheoran2004@gmail.com>
2024-01-09 20:38:25 +0530

    Update splash screen layout with a new animation and improved UI

--- lib/screens/auth_ui/splash_screen.dart
+++ lib/screens/auth_ui/splash_screen.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
+import 'package:swift_cart/controllers/get_user_data_controller.dart';
+import 'package:swift_cart/screens/admin_panel/admin_main_screen.dart';
 import 'package:swift_cart/screens/auth_ui/welcome_screen.dart';
 import 'package:swift_cart/screens/user_panel/main_screen.dart';
 import 'package:swift_cart/utils/app_constant.dart';
@@ -14,17 +17,34 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  User? user = FirebaseAuth.instance.currentUser;
+
   @override
   void initState() {
-    Timer(const Duration(seconds:3), () {
-       Get.offAll(()=>const WelcomeScreen());
+    Timer(const Duration(seconds: 3), () {
+      loggdin(context);
     });
     super.initState();
   }
 
+  Future<void> loggdin(BuildContext context) async {
+    if (user != null) {
+      final GetUserDataController getUserDataController =
+      Get.put(GetUserDataController());
+      var userData =await getUserDataController.getUserData(user!.uid);
+      if(userData[0]['isAdmin']==true){
+        Get.offAll(()=>const AdminMainScreen());
+      }else{
+        Get.offAll(()=>const MainScreen());
+      }
+    } else {
+      Get.to(() => const WelcomeScreen());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-   // final size = MediaQuery.of(context).size;
+    // final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: AppConstant.appScendoryColor,
       appBar: AppBar(
@@ -48,10 +68,9 @@ class _SplashScreenState extends State<SplashScreen> {
               child: Text(
                 AppConstant.appPoweredBy,
                 style: const TextStyle(
-                  color: AppConstant.appTextColor,
-                  fontSize: 12.0,
-                  fontWeight: FontWeight.bold
-                ),
+                    color: AppConstant.appTextColor,
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.bold),
               ),
             )
           ],
