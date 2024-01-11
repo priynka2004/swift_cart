f94222f06d5254c5fd7e9bce6b4a4ccc06f4fd8c
Priyanka Sheoran <priynka.sheoran2004@gmail.com>
2024-01-11 18:27:26 +0530

    create place order service

--- lib/services/place_order_service.dart
+++ lib/services/place_order_service.dart
@@ -5,6 +5,7 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 import 'package:swift_cart/models/order_model.dart';
 import 'package:swift_cart/screens/user_panel/main_screen.dart';
+import 'package:swift_cart/services/genrant_order_id_service.dart';
 import 'package:swift_cart/utils/app_constant.dart';
 
 void placeOrder({
@@ -26,10 +27,78 @@ void placeOrder({
 
       List<QueryDocumentSnapshot> documents = querySnapshot.docs;
 
+      for (var doc in documents) {
+        Map<String, dynamic>? data = doc.data() as Map<String, dynamic>;
+        String orderId = generateOrderId();
+        OrderModel cartModel = OrderModel(
+          productId: data['productId'],
+          categoryId: data['categoryId'],
+          productName: data['productName'],
+          categoryName: data['categoryName'],
+          salePrice: data['salePrice'],
+          fullPrice: data['fullPrice'],
+          productImages: data['productImages'],
+          deliveryTime: data['deliveryTime'],
+          isSale: data['isSale'],
+          productDescription: data['productDescription'],
+          createdAt: DateTime.now(),
+          updatedAt: data['updatedAt'],
+          productQuantity: data['productQuantity'],
+          productTotalPrice: double.parse(data['productTotalPrice'].toString()),
+          customerId: user.uid,
+          status: false,
+          customerName: customerName,
+          customerPhone: customerPhone,
+          customerAddress: customerAddress,
+          customerDeviceToken: customerDeviceToken,
+        );
 
+        for (var x = 0; x < documents.length; x++) {
+          await FirebaseFirestore.instance
+              .collection('orders')
+              .doc(user.uid)
+              .set(
+            {
+              'uId': user.uid,
+              'customerName': customerName,
+              'customerPhone': customerPhone,
+              'customerAddress': customerAddress,
+              'customerDeviceToken': customerDeviceToken,
+              'orderStatus': false,
+              'createdAt': DateTime.now()
+            },
+          );
+
+          await FirebaseFirestore.instance
+              .collection('orders')
+              .doc(user.uid)
+              .collection('confirmOrders')
+              .doc(orderId)
+              .set(cartModel.toMap());
+
+          await FirebaseFirestore.instance
+              .collection('cart')
+              .doc(user.uid)
+              .collection('cartOrders')
+              .doc(cartModel.productId.toString())
+              .delete()
+              .then((value) {
+            print('Delete cart Products $cartModel.productId.toString()');
+          });
+        }
+      }
+
+      print("Order Confirmed");
+      Get.snackbar(
+        "Order Confirmed",
+        "Thank you for your order!",
+        backgroundColor: AppConstant.appMainColor,
+        colorText: Colors.white,
+        duration: const Duration(seconds: 5),
+      );
 
       EasyLoading.dismiss();
-      Get.offAll(() => MainScreen());
+      Get.offAll(() => const MainScreen());
     } catch (e) {
       print("error $e");
     }
