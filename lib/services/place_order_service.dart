841f37b4d393e32ad5bbd1365255347053a8a636
Priyanka Sheoran <priynka.sheoran2004@gmail.com>
2024-01-11 17:50:22 +0530

    implement place order service and genrant order id service

---
+++ lib/services/place_order_service.dart
@@ -0,0 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get/get.dart';
+import 'package:swift_cart/models/order_model.dart';
+import 'package:swift_cart/screens/user_panel/main_screen.dart';
+import 'package:swift_cart/utils/app_constant.dart';
+
+void placeOrder({
+  required BuildContext context,
+  required String customerName,
+  required String customerPhone,
+  required String customerAddress,
+  required String customerDeviceToken,
+}) async {
+  final user = FirebaseAuth.instance.currentUser;
+  EasyLoading.show(status: "Please Wait..");
+  if (user != null) {
+    try {
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('cart')
+          .doc(user.uid)
+          .collection('cartOrders')
+          .get();
+
+      List<QueryDocumentSnapshot> documents = querySnapshot.docs;
+
+
+
+      EasyLoading.dismiss();
+      Get.offAll(() => MainScreen());
+    } catch (e) {
+      print("error $e");
+    }
+  }
+}
